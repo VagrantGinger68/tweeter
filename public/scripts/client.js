3a9d62948259b5eb78673ef3b4ae39dbc792f5f8
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-12 16:16:27 -0400

    added styling for tweets and built functions to dynamically display them

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,74 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
 
+
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $('#tweet-container').append(newTweet);
+    }
+  };
+  
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+  <article class="tweet">
+    <header>
+      <div class="profile">
+        <img src="${tweet.user.avatars}" alt="profile-pic">
+        <span class="tweeter-name">${tweet.user.name}</span>
+      </div>
+      <div>
+        <span class="user-handle">${tweet.user.handle}</span>
+      </div>
+    </header>
+    <div class="tweet-content">
+      ${tweet.content.text}
+    </div>
+    <footer>
+      <span>${tweet.created_at}</span>
+      <div class="tweet-responses">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `);
+    return $tweet;
+  };
+
+
+  renderTweets(data);
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on("input", function(event) {
+    const startingChar = 140;
+    const inputLength = ($(this).val().length);
+    const charCounter = startingChar - inputLength;
+    const counter = $(this).parent().find(".counter");
+
+    counter.text(charCounter);
+
+    if (charCounter < 0) {
+      counter.addClass("red-counter");
+    } else {
+      counter.removeClass("red-counter");
+    }
+  });
+
+});
