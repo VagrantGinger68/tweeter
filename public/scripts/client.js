f7eec1733920ec9c1e77fb05572f1ccd64a309dc
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-13 09:23:47 -0400

    added timeago script and made load tweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-
 
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
@@ -40,7 +14,6 @@ $(document).ready(function() {
     }
   };
   
-
   const createTweetElement = function(tweet) {
     let $tweet = $(`
   <article class="tweet">
@@ -57,7 +30,7 @@ $(document).ready(function() {
       ${tweet.content.text}
     </div>
     <footer>
-      <span>${tweet.created_at}</span>
+      <span>${timeago.format(tweet.created_at)}</span>
       <div class="tweet-responses">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -69,15 +42,18 @@ $(document).ready(function() {
     return $tweet;
   };
 
-
-  renderTweets(data);
-
-
   $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
     const newTweet = $(this).serialize();
     $.post("/tweets/", newTweet);
   });
 
+  const loadTweets = function() {
+    $.get("/tweets/", function(tweets) {
+      renderTweets(tweets);
+    });
+  };
+
+  loadTweets();
 
 });
